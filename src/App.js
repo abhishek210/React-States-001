b34a1f2230fe502831bd471201063cd73886d456
abhishek yadav <abhishekyadav9667@gmail.com>
2023-02-23 13:44:15 +0530

    react-project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-function App() {
+const App=() =>{
+  const [initial,final] = useState(false)
+  const [first,second]=useState(false)
+  function change(){
+    final(!initial);
+    second(!first)
+  }
+  const color1 = initial ? 'black' :'white'
+  const color2=  first ? 'pink' : 'black'
+ 
+  const style1={backgroundColor:color1,color:color2}
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+     <div style={style1} className='mostouter'>
+     <div className='header'>
+      <h1>Overreacted</h1>
+      <button onClick={change} style={{border:"none",backgroundColor:'pink',width:'60px',height:'30px'}}>Toggle</button>
     </div>
-  );
+    <div className='maindiv'>
+     <h1>The Wet Codebase</h1>
+     <p>Sunday 4th,2020 5min read</p>
+     <p className='ptag'>Come waste your time with me</p>
+    </div>
+    <div className='maindiv'>
+     <h1>Goodbye,Clean Code</h1>
+     <p>Friday 6th,2021 10min read</p>
+     <p className='ptag'>Come waste your time with me</p>
+    </div>
+    <div className='maindiv'>
+     <h1>My Decade in Review</h1>
+     <p>Friday 10th,2022 10min read</p>
+     <p className='ptag'>A personal reflection</p>
+    </div>
+    <div className='maindiv'>
+     <h1>What are The React Principle</h1>
+     <p>Monday 10th,2022 10min read</p>
+    </div>
+     </div>
+    </>
+  )
 }
-
 export default App;
